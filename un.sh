2a75731846bd63a4962c647be850f5bfe23507da
z3r0_ID <65604994+MrCimin@users.noreply.github.com>
2020-06-24 09:53:05 +0700

    Add files via upload

---
+++ un.sh
@@ -0,0 +1,7 @@
+#Jangan diubah atau scriptnya error :)
+
+cd /data/data/com.termux/files/home/.termux
+rm -rf termux.properties
+
+#Cuma hapus "termux.properties" masa recode
+#Date : 12-6-2020 22:42
---
+++ up.sh
@@ -0,0 +1,8 @@
+#Jangan di rubah atau scriptnya error :)
+
+cd $HOME
+rm -rf key
+git clone https://github.com/MrCimin/key
+
+#Update make bash doang masa harus recode
+#Date : 17-6-2020 11:52
